05293a23ac57e5b66e57f793e40a6c3214d430f6
Marat <marat.297386@gmail.com>
2025-03-26 20:50:50 +0300

    Update create_db.sql

--- dal/create_db.sql
+++ dal/create_db.sql
@@ -26,18 +26,25 @@ CREATE TABLE Category_clothes (
     category TEXT NOT NULL
 );
 
+-- Создание таблицы Subcategory_clothes
+CREATE TABLE Subcategory_clothes (
+    id_subcategory SERIAL PRIMARY KEY,
+    subcategory TEXT NOT NULL
+);
+
+-- Создание таблицы Subcategory_clothes
+CREATE TABLE Sub_subcategory_clothes (
+    id_subcategory SERIAL PRIMARY KEY,
+    sub_subcategory TEXT NOT NULL
+);
+
 -- Создание таблицы Photo_clothes
 CREATE TABLE Photo_clothes (
     id_clothes SERIAL PRIMARY KEY,
     id_user INT REFERENCES Users(id_user),
     photo_path TEXT NOT NULL,   -- Путь к файлу или URL
+    id_category INT REFERENCES Category_clothes(id_category),  -- Новое поле
     id_subcategory INT REFERENCES Subcategory_clothes(id_subcategory),  -- Новое поле
+    id_sub_subcategory INT REFERENCES Sub_subcategory_clothes(id_sub_subcategory),  -- Новое поле
     is_cut BOOLEAN
-);
-
--- Создание таблицы Subcategory_clothes
-CREATE TABLE Subcategory_clothes (
-    id_subcategory SERIAL PRIMARY KEY,
-    id_category INT REFERENCES Category_clothes(id_category),
-    subcategory TEXT NOT NULL
 );
