5de295f95c6d25c039915380efc55648f93e113b
Marat <marat.297386@gmail.com>
2025-04-03 19:02:33 +0300

    Add and delete photo clothes and photo users for user, add add_photo in catalog, add delete photo in catalog

--- bl/clothes_bl/clothes_bl.py
+++ bl/clothes_bl/clothes_bl.py
@@ -9,11 +9,21 @@ BASE_PHOTO_DIR = os.path.join(os.getcwd(), "photo_clothes")  # Корневая 
 UPLOAD_FOLDER = os.path.join(BASE_PHOTO_DIR, "uploads_clothes")  # Путь для оригинальных изображений
 PROCESSED_FOLDER = os.path.join(BASE_PHOTO_DIR, "processed_images_clothes")  # Путь для обработанных изображений
 
+UPLOAD_FOLDER_CATALOG = os.path.join(BASE_PHOTO_DIR,
+                                     "uploads_clothes_catalog")  # Путь для оригинальных изображений каталога
+PROCESSED_FOLDER_CATALOG = os.path.join(BASE_PHOTO_DIR,
+                                        "processed_images_clothes_catalog")  # Путь для обработанных изображений
+
 # Создаем папки, если их нет
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 if not os.path.exists(PROCESSED_FOLDER):
     os.makedirs(PROCESSED_FOLDER)
+
+if not os.path.exists(UPLOAD_FOLDER_CATALOG):
+    os.makedirs(UPLOAD_FOLDER_CATALOG)
+if not os.path.exists(PROCESSED_FOLDER_CATALOG):
+    os.makedirs(PROCESSED_FOLDER_CATALOG)
 
 
 def remove_background_clothes(input_path):
@@ -31,4 +41,22 @@ def remove_background_clothes(input_path):
         return output_filename
     except Exception as error:
         logging.error(f"Ошибка обработки изображения: {error}")
+        return None
+
+
+def remove_background_clothes_catalog(input_path):
+    """
+    Удаляет фон из изображения для каталога и сохраняет результат.
+    """
+    try:
+        input_image = Image.open(input_path)
+        output_image = remove(input_image)
+
+        output_filename = f"{uuid.uuid4().hex}.png"
+        output_path = os.path.join(PROCESSED_FOLDER_CATALOG, output_filename)
+
+        output_image.save(output_path)
+        return output_filename
+    except Exception as error:
+        logging.error(f"Ошибка обработки изображения для каталога: {error}")
         return None
