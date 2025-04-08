4b439e9a33c9c92564a43752d1e2866dac7071a6
Marat <marat.297386@gmail.com>
2025-04-08 21:40:31 +0300

    Comment routes_for_clothes.py

--- routes_for_clothes/__init__.py
+++ routes_for_clothes/__init__.py
@@ -1,7 +1,7 @@
 __all__ = ("clothes_blueprint",)
 
 from flask import Blueprint
-# from .routes_for_clothes import clothes_blueprint as clothes
+from .routes_for_clothes import clothes_blueprint as clothes  # Он скоро удалится
 from .add_photos import add_photos
 from .recovery_photos import recovery_photos
 from .clothes_fetch_routes import get_wardrobe_catalog
@@ -10,3 +10,4 @@ clothes_blueprint = Blueprint("clothes_main", __name__, url_prefix='/clothes')
 clothes_blueprint.register_blueprint(get_wardrobe_catalog)
 clothes_blueprint.register_blueprint(add_photos)
 clothes_blueprint.register_blueprint(recovery_photos)
+clothes_blueprint.register_blueprint(clothes)  # Он скоро удалится
