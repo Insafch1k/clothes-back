58260567a72a9919bfe66d450174dda5ff69db96
Marat <marat.297386@gmail.com>
2025-04-09 20:30:04 +0300

    Add file putting_on_clothes.py

--- routes_for_background/__init__.py
+++ routes_for_background/__init__.py
@@ -1,14 +1,14 @@
 __all__ = ("background_blueprint",)
 
 from flask import Blueprint
-from .background_routes import background_blueprint as background
+from .background_routes import background_blueprint as background  # Скоро удалится
 from .recovery_photos import recovery_photos
 from .delete_photos import delete_photos
 from .add_photos import add_photos
 from .background_fetch_routes import get_user_photos
 
 background_blueprint = Blueprint("background_main", __name__, url_prefix='/human')
-background_blueprint.register_blueprint(background)
+background_blueprint.register_blueprint(background)  # Скоро удалится
 background_blueprint.register_blueprint(recovery_photos)
 background_blueprint.register_blueprint(delete_photos)
 background_blueprint.register_blueprint(add_photos)
