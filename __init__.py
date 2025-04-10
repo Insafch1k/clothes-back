db7e7e99edc290dfe5957db0f5be85cfa84deb95
BakievRuslan-rb <rbakiev279@gmail.com>
2025-04-10 20:47:42 +0300

    adding register

--- __init__.py
+++ __init__.py
@@ -6,8 +6,10 @@ main_blueprint = Blueprint('main', __name__)
 # Импорт и регистрация дочерних Blueprints
 from routes_for_clothes import clothes_blueprint
 from routes_for_background import background_blueprint
+from routes_for_users import users_blueprint
 
 main_blueprint.register_blueprint(clothes_blueprint)
 main_blueprint.register_blueprint(background_blueprint)
+main_blueprint.register_blueprint(users_blueprint)
 
 __all__ = ('main_blueprint',)
